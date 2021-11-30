b041139f6d1a6b60fb0fbeeab327b14459309a87
stefano28 <stefanoleggio28@gmail.com>
2021-12-01 00:50:03 +0100

    query by keywords and profiles

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tweepy as tw
 import json
 from dotenv import load_dotenv
 import os
+from datetime import timezone
+import datetime
 
 
 #
@@ -9,10 +11,8 @@ import os
 #
 def connection_to_api():
     auth = tw.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_KEY_SECRET"))
-    auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
     api = tw.API(auth, wait_on_rate_limit=True)
-    client = tw.Client(bearer_token= os.getenv("BAERER_TOKEN"))
-    return api, client
+    return api
 
 #
 #   Create a twitter member list (if is needed)
@@ -25,14 +25,18 @@ def list_creation(api, profiles):
 
 
 #
-#   Load profiles screen name from profiles.txt
+#   Load profiles screen name from profiles.txt and words from words.txt
 #
-def load_profiles():
+def load_filters():
     profiles = []
-    file = open("profiles.txt", "r")
-    for line in file:
-        profiles.append(line)
-    return profiles
+    keywords = []
+    file_profiles = open("filters/profiles.txt", "r")
+    file_keywords = open("filters/keywords.txt", "r")
+    for line in file_profiles:
+        profiles.append(line.rstrip('\n'))
+    for line in file_keywords:
+        keywords.append(line.rstrip('\n'))
+    return profiles, keywords
 
 
 
@@ -40,13 +44,24 @@ if __name__ == "__main__":
 
     load_dotenv()
 
-    api, client = connection_to_api()
+    api = connection_to_api()
 
-    profiles = load_profiles()
+    profiles, keywords = load_filters()
 
-    #TODO api.search_full_archive(label="development", query = "")
+    keywords_query = "("
+
+    for keyword,i in zip(keywords, range(len(keywords))):
+        keywords_query += keyword
+        if(i<len(keywords)-1):
+            keywords_query += " OR "
+    keywords_query += ")"
+
+    fromDate = datetime.date.today() - datetime.timedelta(1)
+
+    file_results = open("output/out.txt", "w")
 
     for profile in profiles:
-        for status in tw.Cursor(api.user_timeline, screen_name = profile, exclude_replies = True, include_rts = False, tweet_mode="extended").items(2):
-            print(status.full_text)
-            print()
+        query = keywords_query + " from: " + profile
+        for status in tw.Cursor(api.search_30_day,label=os.getenv("DEV_ENVIRONMENT") ,fromDate= fromDate.strftime("%Y%m%d%H%M"), query= query).items():
+            file_results.write(json.dumps(status._json, indent=4))
+            file_results.write("\n\n\n ---------------- \n\n\n")
