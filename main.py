76e10e150716f1429965ace83f8490c8f8e6074e
stefano28 <stefanoleggio28@gmail.com>
2021-12-03 21:36:54 +0100

    moving to apiV2

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import os
 from datetime import timezone
 import datetime
 import time
+from collections import namedtuple
 
 #
 #   Limit parameters for testing (if you are not using a premium account)
 #
 
-REPLIES_LIMIT = 20
+RESULTS_LIMIT = 10
+RESULT_PER_PAGE = 11
 
 
 #
@@ -18,8 +20,9 @@ REPLIES_LIMIT = 20
 #
 def connection_to_api():
     auth = tw.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_KEY_SECRET"))
+    client = tw.Client(bearer_token=os.getenv("BEARER_TOKEN"))
     api = tw.API(auth, wait_on_rate_limit=True)
-    return api
+    return api,client
 
 
 #
@@ -40,30 +43,19 @@ def load_filters():
 #
 #   Collect the replies to a tweet
 #
-def get_replies(profile,tweet_id,api):
-
-    # Get all the replies to a profile
-
-    data = tw.Cursor(api.search_tweets, q='to:{}'.format(profile),since_id=tweet_id, tweet_mode='extended').items(REPLIES_LIMIT)
-    replies = []
-
-    # For each reply to a profile let's check if is a reply to the tweet with tweet_id
-    # If yes let's add it to the replies list
-
-    for reply in data:
-        if not hasattr(reply, 'in_reply_to_status_id_str'):
-            continue
-        if reply.in_reply_to_status_id == tweet_id:
-            replies.append(reply)
- 
-    return replies
+def get_replies(conversation_id):
+    for reply in tw.Paginator(client.search_recent_tweets, query="conversation_id:"+str(conversation_id), max_results=10).flatten(limit=RESULT_PER_PAGE):
+        parsed = {}
+        parsed['id'] = reply.id
+        parsed['text'] = reply.text
+        file_replies.write(json.dumps(parsed, indent=4))
 
 
 if __name__ == "__main__":
 
     load_dotenv()
 
-    api = connection_to_api()
+    api,client = connection_to_api()
 
     # Loading of profiles screen names and the keywords
     
@@ -74,18 +66,6 @@ if __name__ == "__main__":
     print("[*] profiles loaded: ", len(profiles))
     print("[*] keywords loaded: ", len(keywords))
 
-    # Quey building: (keyword1 OR keyword2 OR ...)
-
-    keywords_query = "("
-
-    for keyword,i in zip(keywords, range(len(keywords))):
-        keywords_query += keyword
-        if(i<len(keywords)-1):
-            keywords_query += " OR "
-
-    keywords_query += ")"
-
-    # Set the fromDate as yesterday
 
     fromDate = datetime.date.today() - datetime.timedelta(1)
 
@@ -96,33 +76,21 @@ if __name__ == "__main__":
     file_tweets = open("output/tweets.json", "w")
     file_replies = open("output/replies.json", "w")
 
+    conversations = []
+
+    # For each profile I get all the tweets from a certain date
 
     for profile in profiles:
-        print("[*] collecting tweets of: ", profile)
+        user_id = client.get_user(username=profile)[0].id
+        for tweet in tw.Paginator(client.get_users_tweets,id = user_id, tweet_fields="conversation_id", max_results=RESULTS_LIMIT).flatten(limit=RESULT_PER_PAGE):
+            for keyword in keywords:
+                if keyword in tweet.text:
+                    parsed = {}
+                    parsed['id'] = tweet.id
+                    parsed['conversation_id'] = tweet.conversation_id
+                    parsed['text'] = tweet.text
+                    file_tweets.write(json.dumps(parsed, indent=4))
+                    conversations.append(tweet.conversation_id)
 
-        # Query building: (keyword1 OR keyword2 OR ...) from profile_name
-
-        query = keywords_query + " from: " + profile
-
-        # Get all the tweets that contains one or more keywords from each profile in filters/profiles.txt
-
-        for tweet in tw.Cursor(api.search_full_archive,label=os.getenv("DEV_ENVIRONMENT") ,fromDate= fromDate.strftime("%Y%m%d%H%M"), query= query).items():
-
-            print("[*] loading replies for tweet_id: ", tweet.id, "in ", profile)
-
-            # For each tweet let's get all the replies associated
-
-            replies = get_replies(profile,tweet.id,api)
-
-            print("> replies colected: ", len(replies))
-
-            # Print all the replies in output/replies.json
-
-            for reply in replies:
-                file_replies.write(json.dumps(reply._json, indent=4))
-                file_replies.write("\n\n\n")
-
-            # Add the tweet in output/tweets.json
-
-            file_tweets.write(json.dumps(tweet._json, indent=4))
-            file_tweets.write("\n\n\n")
+    for conversation_id in conversations:
+        replies = get_replies(conversation_id)
