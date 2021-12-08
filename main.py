6ac06133d950f2dce62e0a82b70a8bd7013acc51
stefano28 <stefanoleggio28@gmail.com>
2021-12-08 11:36:35 +0100

    optimizing retweet collection

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from datetime import datetime as dt,timezone
 import datetime
 import time
 import pandas as pd
+import logging
+from rich.console import Console
+from rich.table import Column, Table
 
 #
-#   Limit parameters for testing (if you are not using a premium account)
+#   Limit parameters for testing (if you are not using an academic account)
 #
 
-RESULTS_LIMIT = 10
-
+RESULTS_LIMIT = 1
 
 #
 #   Method for establish a connection whit Twitter API
 #
 def connection_to_api():
-    auth = tw.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_KEY_SECRET"))
-    api = tw.API(auth, wait_on_rate_limit=True)
-    client = tw.Client(bearer_token=os.getenv("BEARER_TOKEN"))
-    return client, api
+    client = tw.Client(bearer_token=os.getenv("BEARER_TOKEN"), wait_on_rate_limit=True)
+    return client
 
 
 #
@@ -38,13 +38,103 @@ def load_filters():
         keywords.append(line.rstrip('\n'))
     return profiles, keywords
 
-def log_print(msg):
-    parsed = "[" + str(dt.now().strftime("%Y-%m-%d %H:%M:%S")) + "] " + msg.translate({ord('\n'): None}) + "\n"
-    file_logs.write(parsed)
-    print(msg)
 
+#
+#   This method collects all the quotes retweets of a tweet
+#   The output is saved in output/retweets.json
+#
+def get_retweets(profile, tweet_id):
+
+    logging.info("Collecting retweets of " + str(tweet_id) + "...")
+
+    rtw_count = 0
+
+    url = "https://twitter.com/" + profile + "/status/" + str(tweet_id)
+
+    for retweet in tw.Paginator(client.search_recent_tweets,query='url: "{}"'.format(url)  + " is:quote", tweet_fields=["author_id","created_at","public_metrics","entities","geo","lang","referenced_tweets"]).flatten(RESULTS_LIMIT):
+        belongs_to_tweet = False
+        refs = []
+        for el in retweet.referenced_tweets:
+            if el.id == tweet_id:
+                belongs_to_tweet = True
+            rt = {}
+            rt['type'] = el.type
+            rt['id'] = el.id
+            refs.append(rt)
+
+        if(belongs_to_tweet):
+            parsed = {}
+            parsed['id'] = retweet.id
+            parsed['author_id'] = retweet.author_id
+            parsed['tweet_id'] = tweet.id
+            parsed['created_at'] = str(retweet.created_at)
+            parsed['text'] = retweet.text
+            parsed['public_metrics'] = retweet.public_metrics
+            parsed['entities'] = retweet.entities
+            parsed['geo'] = retweet.geo
+            parsed['lang'] = retweet.lang
+            #TODO Send parsed dict to mongo db (retweet)
+            file_retweets.write(json.dumps(parsed, indent=4))
+            rtw_count += 1
+    
+    logging.info("[*] Retweets collected: " + str(rtw_count))
+
+    return rtw_count
+
+
+#
+#   This method collects all the directly replies of a tweet
+#   The output is saved in output/replies.json
+#
+def get_replies(tweet_id):
+
+    logging.info("Collecting replies of " + str(tweet_id) + "...")
+
+    rpl_count = 0
+
+    for reply in tw.Paginator(client.search_recent_tweets,query="conversation_id:{}".format(tweet_id),tweet_fields=["author_id","created_at","public_metrics","entities","geo","lang","referenced_tweets"]).flatten(RESULTS_LIMIT):
+        refs = []
+        answer_to_tweet = False
+
+        # This condition filter only the direct replies to the tweet (excluding the replies of replies)
+
+        for el in reply.referenced_tweets:
+            if(el.id == tweet_id):
+                answer_to_tweet = True
+            rt = {}
+            rt['type'] = el.type
+            rt['id'] = el.id
+            refs.append(rt)
+
+        if(answer_to_tweet):
+            parsed = {}
+            parsed['id'] = reply.id
+            parsed['author_id'] = reply.author_id
+            parsed['tweet_id'] = tweet_id
+            parsed['created_at'] = str(reply.created_at)
+            parsed['text'] = reply.text
+            parsed['public_metrics'] = reply.public_metrics
+            parsed['entities'] = reply.entities
+            parsed['geo'] = reply.geo
+            parsed['lang'] = reply.lang
+            #TODO Send parsed dict to mongo db (reply)
+            file_replies.write(json.dumps(parsed, indent=4))
+            rpl_count += 1
+
+    logging.info("[*] Replies collected: " + str(rpl_count))
+
+    return rpl_count
 
 if __name__ == "__main__":
+
+
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("Profile", style="dim")
+    table.add_column("Tweet")
+    table.add_column("Retweets collected", justify="right")
+    table.add_column("Replies collected", justify="right")
+
+    console = Console()
 
     start_time = time.time()
 
@@ -54,31 +144,37 @@ if __name__ == "__main__":
 
     load_dotenv()
 
+    logger = logging.getLogger('spam_application')
+
+    logging.basicConfig(filename='.appdata/app.log', level=logging.INFO, filemode='w', format="[" + str(datetime.datetime.now(timezone.utc).replace(microsecond=0)) + "] "+'%(name)s - %(levelname)s - %(message)s')
+
     file_state = open(".appdata/state.json", "w")
-    file_logs = open(".appdata/logs.txt", "w")
     file_tweets = open("output/tweets.json", "w")
     file_replies = open("output/replies.json", "w")
     file_retweets = open("output/retweets.json", "w")
 
-    tweets_id = []
+    total_tw_count = 0
 
-    client,api= connection_to_api() # API authentication
+    total_rpl_count = 0
+
+    total_rtw_count = 0
+
+    client = connection_to_api() # API authentication
     
     profiles, keywords = load_filters() # Loading of profiles screen names and the keywords
 
     fromDate = datetime.datetime.now(timezone.utc).replace(microsecond=0) - datetime.timedelta(1)
 
-    log_print("\n\nTweets collector script\n\n")
-    log_print("[INFO] profiles loaded: " + str(len(profiles)))
-    log_print("[INFO] keywords loaded: " + str(len(keywords)))
-    log_print("[INFO] start date: " + fromDate.strftime("%Y%m%d%H%M"))
-    print("\n")
+    logging.info("Starting Tweets collector script")
+    logging.info("Profiles loaded: " + str(len(profiles)))
+    logging.info("Keywords loaded: " + str(len(keywords)))
+    logging.info("Start date: " + fromDate.isoformat())
 
     #
     #   Algorithm
     #
 
-    # Tweets collection (APIv2)
+    # Tweets collection
 
     for profile in profiles:
 
@@ -86,9 +182,9 @@ if __name__ == "__main__":
 
         user_id = client.get_user(username=profile)[0].id
 
-        log_print("Collecting tweets of " + profile + "...")
+        logging.info("Collecting tweets of " + profile + "...")
 
-        for tweet in tw.Paginator(client.get_users_tweets,id = user_id, tweet_fields=["author_id","created_at","public_metrics","entities","geo","lang"], exclude=["retweets", "replies"],start_time=fromDate.isoformat()).flatten(limit=RESULTS_LIMIT):
+        for tweet in tw.Paginator(client.get_users_tweets,id = user_id, tweet_fields=["author_id","created_at","public_metrics","entities","geo","lang"], exclude=["retweets", "replies"],start_time=fromDate.isoformat()).flatten(RESULTS_LIMIT):
             for keyword in keywords:
                 if keyword.casefold() in tweet.text.casefold():
                     parsed = {}
@@ -101,91 +197,61 @@ if __name__ == "__main__":
                     parsed['geo'] = tweet.geo
                     parsed['lang'] = tweet.lang
                     file_tweets.write(json.dumps(parsed, indent=4))
-                    tweets_id.append(tweet.id)
+                    #TODO Send parsed dict to mongo db (tweet)
 
-                    end_time_retweet = (datetime.datetime.fromisoformat(tweet.created_at.isoformat()) + datetime.timedelta(days = 7, minutes = 1)).isoformat()
+                    logging.info("[*] Tweet " + str(tweet.id) +  " collected")
 
-                    now_time = (datetime.datetime.now(timezone.utc).replace(microsecond=0) - datetime.timedelta(minutes = 1)).isoformat()
+                    replies_cnt = get_replies(tweet.id)
 
-                    if(end_time_retweet >= now_time):
-                        end_time_retweet = now_time
+                    retweets_cnt = get_retweets(profile,tweet.id)
 
+                    os.system('cls' if os.name == 'nt' else 'clear')
 
-                    log_print("Collecting retweets of " + str(tweet.id) + "...")
+                    text_to_print = ''
 
-                    rtw_count = 0
-                    
-                    for retweet in tw.Paginator(client.search_recent_tweets,query="url:" + profile + " is:quote", start_time= tweet.created_at.isoformat(),end_time = end_time_retweet,tweet_fields=["author_id","created_at","public_metrics","entities","geo","lang","referenced_tweets"]).flatten(limit=RESULTS_LIMIT):
-                        belongs_to_tweet = False
-                        refs = []
-                        for el in retweet.referenced_tweets:
-                            if(el.id == tweet.id):
-                                belongs_to_tweet = True
-                            rt = {}
-                            rt['type'] = el.type
-                            rt['id'] = el.id
-                            refs.append(rt)
+                    if(len(tweet.text)<25):
+                        text_to_print = tweet.text
+                    else:
+                        text_to_print = tweet.text[:25] + "..."
 
-                        if(belongs_to_tweet):
-                            rtw_count += 1
-                            parsed['id'] = retweet.id
-                            parsed['author_id'] = retweet.author_id
-                            parsed['tweet_id'] = tweet.id
-                            parsed['created_at'] = str(retweet.created_at)
-                            parsed['text'] = retweet.text
-                            parsed['public_metrics'] = retweet.public_metrics
-                            parsed['entities'] = retweet.entities
-                            parsed['geo'] = retweet.geo
-                            parsed['lang'] = retweet.lang
-                            parsed['referenced_tweets'] = refs                        
-                            file_retweets.write(json.dumps(parsed, indent=4))
-                        
-                    log_print("[*] retweets with quotes collected: " + str(rtw_count) + "\n")
+                    table.add_row(
+                        profile,
+                        str(text_to_print.replace('\n', ' ')),
+                        str(retweets_cnt),
+                        str(replies_cnt),
+                    )
 
-    log_print("[*] total tweets collected: " + str(len(tweets_id)))
-    print("\n")
+                    console.print(table)
 
-    total_rpl_count = 0
-
-    for tweet_id in tweets_id:
-
-        # Replies collection (APIv2)
-        
-        log_print("Collecting replies for tweet_id: " + str(tweet_id) + "...")
-        
-        rpl_count = 0
-
-        for reply in tw.Paginator(client.search_recent_tweets,query="conversation_id:{}".format(tweet_id),tweet_fields=["author_id","created_at","public_metrics","entities","geo","lang"]).flatten(limit=RESULTS_LIMIT):
-            parsed = {}
-            parsed['id'] = reply.id
-            parsed['author_id'] = reply.author_id
-            parsed['tweet_id'] = tweet_id
-            parsed['created_at'] = str(reply.created_at)
-            parsed['text'] = reply.text
-            parsed['public_metrics'] = reply.public_metrics
-            parsed['entities'] = reply.entities
-            parsed['geo'] = reply.geo
-            parsed['lang'] = reply.lang
-            file_replies.write(json.dumps(parsed, indent=4))
-            rpl_count += 1
-
-        total_rpl_count += rpl_count
-
-        log_print("[*] replies collected: " + str(rpl_count) + "\n")
+                    total_tw_count += 1
+                    total_rpl_count += replies_cnt
+                    total_rtw_count += retweets_cnt
 
     end_time = time.time()
 
-    log_print("---- Total execution time:" + str(end_time-start_time) + " ----")
+    exec_time = time.gmtime(end_time-start_time)
+    exec_time = time.strftime("%H:%M:%S",exec_time)
 
+    logging.info("Collection finished")
+    logging.info("[*] Tweets collected: " + str(total_tw_count))
+    logging.info("[*] Retweets collected: " + str(total_rtw_count))
+    logging.info("[*] Replies collected: " + str(total_rpl_count))
+    logging.info("Execution time: " + str(exec_time))
+    print("\n")
+    print("Tweets collected: ",total_tw_count)
+    print("Retweets collected: ",total_rtw_count)
+    print("Replies collected: ",total_rpl_count)
+    print("Execution time: " + str(exec_time))
+    print("\n")
     state = {}
-    state['last_run'] = datetime.date.today().strftime("%Y-%m-%dT%H:%M:%SZ")
-    state['total_tweets_collected'] = len(tweets_id)
+    state['last_run'] = str(datetime.datetime.now(timezone.utc).replace(microsecond=0))
+    state['total_tweets_collected'] = total_tw_count
     state['total_replies_collected'] = total_rpl_count
-    state['execution_time'] = end_time - start_time
+    state['total_retweets_collected'] = total_rtw_count
+    state['execution_time'] = exec_time
     file_state.write(json.dumps(state, indent=4))
 
     file_state.close()
-    file_logs.close()
     file_tweets.close()
     file_replies.close()
     file_retweets.close()
