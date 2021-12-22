359d1f817f46a6840a683f0b532ae0d3d3d47786
stefano28 <stefanoleggio28@gmail.com>
2021-12-22 22:30:24 +0100

    update

--- TweetsCollector/test.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-bashCommand = "node ../TweetsToPsql/insert.js output/replies.json"
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
-print(output)
